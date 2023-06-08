20e4cc390a8df1740172fa549eaadd1536e83f5b
Kavindu Gayan <kavrup93@gmail.com>
2023-06-09 00:46:00 +0530

    Add css styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCopy } from '@fortawesome/free-solid-svg-icons';
+import { BeatLoader } from 'react-spinners';
 
 function App() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [result, setResult] = useState(null)
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [pdfName, setPdfName] = useState('')
+  const [loading, setLoading] = useState(false);
 
   const onFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
+    setPdfName(event.target.files[0].name)
+    setIsDisabled(false)
   };
+
+  const hideUploadButton = {
+    display: 'none',
+  }
 
   const onFileUpload = async () => {
     try {
+      setLoading(true)
       const formData = new FormData();
       formData.append('file', selectedFile);
 
@@ -24,6 +33,7 @@ function App() {
 
       console.log('File uploaded successfully:', response.data);
       setResult(response.data.result)
+      setLoading(false)
     } catch (error) {
       console.error('Error uploading file:', error);
     }
@@ -34,24 +44,50 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1 className="title">PDF File Uploader</h1>
-      <div className="file-input-container">
-        <input type="file" accept=".pdf" onChange={onFileChange} />
-        {selectedFile && (
-          <button className="upload-button" onClick={onFileUpload}>
-            Upload File
-          </button>
-        )}
+    <>
+      <div className="background">
+        <div class="header">
+          <div class="header-font">
+            Intelligent Document Processing
+          </div>
+        </div>
       </div>
-      <div className="result-container">
-        {result && <p className="result-text">{result}<button className="copy-button" onClick={copyToClipboard}>
-              <FontAwesomeIcon icon={faCopy} />
-            </button></p>  
-        }
-      
+
+      {loading && (
+        <div
+          style={{
+            position: 'fixed',
+            top: '15%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+          }}
+        >
+          <BeatLoader color="#000000" />
+        </div>)
+      }
+
+      {result &&
+        <div class="result-url-box">
+          <a class="result-url"
+            href="https://docs.google.com/spreadsheets/d/1KmNit3g3tW8JX-8a04bpfjJN_b6jMjKZ_gaLcMkoCL0/edit?usp=sharing"
+            target="_blank">https://docs.google.com/spreadsheets/d/1KmNit3g3tW8JX-8a04bpfjJN_b6jMjKZ_gaLcMkoCL0/edit?usp=sharing</a>
+        </div>
+      }
+
+      <div class="main-rectangle">
+        <div class="upload-pdf-font">
+          Upload PDF File
+        </div>
+        <label for="file-upload" class="upload-image"></label>
+        <input type="file" id="file-upload" class="upload-image" accept=".pdf" style={hideUploadButton} onChange={onFileChange} />
+        <div className='upload-pdf-name'>{pdfName}</div>
+        <div class="notification">Drag & drop or browse</div>
+        <button class="button-upload" onClick={onFileUpload} disabled={isDisabled}>
+          <div class="button-text" >Upload </div>
+        </button>
+        <div class="upload-instruction">Upload PDF File To Process Your Document.</div>
       </div>
-    </div>
+    </>
 
   );
 }
